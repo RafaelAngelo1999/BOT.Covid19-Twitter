0677d0a05f91d7137c40245159620a0a8c3eb835
Rafael <rafaelangelowow@gmail.com>
2022-03-03 20:15:11 -0300

    alter instance

--- src/shared/utils/ClientTwitterInstance.ts
+++ src/shared/utils/ClientTwitterInstance.ts
@@ -1,13 +1,13 @@
 import { TwitterApi } from "twitter-api-v2";
 import "dotenv/config";
 
-const clientTwitterInstanceV1 = new TwitterApi({
+const clientTwitterInstance = new TwitterApi({
   appKey: String(process.env.BOT_CONSUMER_KEY),
   appSecret: String(process.env.BOT_CONSUMER_SECRET),
   accessToken: String(process.env.BOT_ACCESS_KEY),
   accessSecret: String(process.env.BOT_ACCESS_TOKEN),
 });
 
-clientTwitterInstanceV1.readWrite;
+clientTwitterInstance.readWrite;
 
-export default clientTwitterInstanceV1;
+export default clientTwitterInstance;
