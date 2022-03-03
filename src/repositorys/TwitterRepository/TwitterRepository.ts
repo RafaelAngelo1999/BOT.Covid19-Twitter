93d4d95ef5b965b9b2d1f8f98c4135f0de37fb9d
Rafael <rafaelangelowow@gmail.com>
2022-03-03 19:14:19 -0300

    ajuste class

--- src/repositorys/TwitterRepository/TwitterRepository.ts
+++ src/repositorys/TwitterRepository/TwitterRepository.ts
@@ -1,35 +1,35 @@
 import clientTwitterInstance from "../../shared/utils/clientTwitterInstance";
 
-class TwitterRepository {
-  retweetPostagemPorId = async (idPostagem: string) => {
+const TwitterRepository = {
+  retweetPostagemPorId: async (idPostagem: string) => {
     return await clientTwitterInstance.v2
       .retweet(String(process.env.TWITTER_ID), idPostagem)
       .then((resposta) => resposta && resposta.data)
       .catch((erro) => erro);
-  };
+  },
 
-  likePostagemPorId = async (idPostagem: string) => {
+  likePostagemPorId: async (idPostagem: string) => {
     return await clientTwitterInstance.v2
       .like(String(process.env.TWITTER_ID), idPostagem)
       .then((resposta) => resposta && resposta.data)
       .catch((erro) => erro);
-  };
+  },
 
-  comentarioPostagemPorId = async (comentario: string, idPostagem: string) => {
+  comentarioPostagemPorId: async (comentario: string, idPostagem: string) => {
     return await clientTwitterInstance.v2
       .reply(comentario, idPostagem)
       .then((resposta) => resposta && resposta.data)
       .catch((erro) => erro);
-  };
+  },
 
-  obterDadosTwitterPorFiltro = async (filtro: string) => {
+  obterDadosTwitterPorFiltro: async (filtro: string) => {
     return await clientTwitterInstance.v1
       .filterStream({
         track: filtro,
       })
       .then((resposta) => resposta)
       .catch((erro) => erro);
-  };
-}
+  },
+};
 
-export default new TwitterRepository();
+export default TwitterRepository;
